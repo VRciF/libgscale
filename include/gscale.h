d4cb61004be84e152fa44a68ba97aa0593d4b58c
NAME <E@MAIL.COM>
2012-11-06 12:44:35 +0100

    reset project for c++

--- include/gscale.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GSCALE_H
-#define _GSCALE_H  1
-
-
-
-#endif
--- src/backend/berkleysockets/udp.h
+++
@@ -1,8 +0,0 @@
-#ifndef BERKLEYSOCKETS_UDP_H
-#define BERKLEYSOCKETS_UDP_H
-
-#include "types.h"
-
-void GScale_Backend_BerkleySockets_UDP_Broadcast(GScale_Backend *_this);
-
-#endif
